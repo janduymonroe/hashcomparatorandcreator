948ca3c9883d980eae6fd40f0d16eb920586ba4f
janduy <janduymonroe@gmail.com>
2021-07-05 13:34:02 -0300

    Add a different hash types to create files hashs

--- hc.py
+++ hc.py
@@ -2,7 +2,7 @@ import hashlib
 from tkinter.filedialog import askopenfilename
 
 print("-"*60)
-print("WELCOME TO HASH SHA256 COMPARATOR AND GENERATOR")
+print("WELCOME TO HASH COMPARATOR AND GENERATOR")
 print("-"*60)
 
 def main(select):
@@ -42,19 +42,28 @@ def hashcomparer():
 
 
 def showhash():
+    select = input(''' ### CHOOSE THE HASH ### 
+        MD5
+        SHA1
+        SHA256
+        SHA512:  ''')
+    hashtypes = ['MD5', 'SHA1', 'SHA256', 'SHA512']
+    if select.upper() not in hashtypes:
+        print("Error: Choose a listed hash.")
+        showhash()
 
     while True:
         try:
-            print("Choose the archive!")
+            print("Choose the archive!\n")
             archive1 = askopenfilename()
 
-            hash1 = hashlib.new('SHA256')
+            hash1 = hashlib.new(select.upper())
             hash1.update(open(archive1, 'rb').read())
             break
         except:
             print("You need choose one file.")
 
-    print(f"The archive {archive1} 'SHA256' hash is: \n\n", hash1.hexdigest())
+    print(f"The archive {archive1} '{select.upper()}' hash is: \n\n", hash1.hexdigest())
 
 
 if __name__ == "__main__":
